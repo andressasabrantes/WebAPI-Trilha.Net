ad8aba85030468e3eab0273be2d4fb75f46dccb1
Andressa Abrantes <andressasabrantes@gmail.com>
2023-08-02 22:35:06 -0300

    Main: Web API Obter Hora e Data e Obter Usuário

---
+++ api-introducao/Controllers/UsuarioController.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace api_introducao.Controllers
+{
+    [ApiController]
+    [Route("[Controller]")]
+    public class UsuarioController : ControllerBase
+    {
+        [HttpGet("ObterDataHoraAtual")]
+        public IActionResult ObterDataHora()
+        {
+            var obj = new
+            {
+                Data = DateTime.Now.ToLongDateString(),
+                Hora = DateTime.Now.ToShortTimeString()
+            };
+
+            return Ok(obj);
+        }
+
+        [HttpGet("Apresentar/{nome}")]
+        public IActionResult Apresentar(string nome)
+        {
+            var mensagem = $"Olá {nome}, seja muito bem-vindo(a)!";
+            return Ok(new { mensagem } );
+        }
+    }
+}
